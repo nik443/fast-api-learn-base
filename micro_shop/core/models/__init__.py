129f84be68f400bdbfde77acb4278c7fd5951201
nik443 <nikhram301@gmail.com>
2024-11-14 08:44:44 +1000

    init

--- micro_shop/core/models/__init__.py
+++ micro_shop/core/models/__init__.py
@@ -1,5 +1,18 @@
-__all__ = ("Base", "Product", "DatabaseHelper", "db_helper")
+# Здесь обязательно нужно указать модели таблиц, с которыми должен работать alembic
+
+__all__ = (
+    "Base",
+    "Product",
+    "Post",
+    "User",
+    "Profile",
+    "DatabaseHelper",
+    "db_helper",
+)
 
 from micro_shop.core.models.base import Base
 from micro_shop.core.models.product import Product
+from micro_shop.core.models.user import User
+from micro_shop.core.models.post import Post
+from micro_shop.core.models.profile import Profile
 from micro_shop.core.models.db_helper import DatabaseHelper, db_helper
